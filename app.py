8b13b55b930cb6845a327208d91d7ef47ed359aa
Sourish Panja <107027619+Sourish-07012003@users.noreply.github.com>
2024-11-23 11:55:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.htmll")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
